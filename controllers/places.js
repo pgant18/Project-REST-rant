e5a57927b050c2f317d3a802751ab69d5df85c48
pgant18 <pgant18@gmail.com>
2022-06-28 17:50:29 -0500

    Adding Post Method

--- controllers/places.js
+++ controllers/places.js
@@ -21,8 +21,14 @@ router.get('/new', (req, res) => {
     res.render('places/new')
 })
 
+router.post('/', (req, res) => {
+
+})
 router.get('/:id', (req, res) => {
     res.render('/places/:id')
 })
-
+router.post ('/places', (req, res) => {
+// Get data from New Post
+    res.json({'code':"new_Form"})
+});
 module.exports = router
